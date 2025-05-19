295a1cbef9e96030a53bf24fb9cd44f59b2ce91d
myroslav <myroslav@clixsy.com>
2025-05-19 16:09:09 -0700

    task1

---
+++ src/main/java/pl/edu/vistula/fist_project_spring/FistProjectSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.fist_project_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FistProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FistProjectSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/vistula/fist_project_spring/FistProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.fist_project_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FistProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
